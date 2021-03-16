4b36abaa1f49f1037f012287daa72daaccea7511
Tammo Stein <tammo.stein@tu-braunschweig.de>
2021-03-16 15:36:55 +0100

    Add Arduino

---
+++ Arduino/yamaha3Darduino/yamaha3Darduino/yamaha3Darduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		yamaha3Darduino.ino
+ Created:	16.03.2021 15:34:44
+ Author:	stein
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
